4bccd608374f2192d41e170c7760b64a4759909e
Upendra Lachipatula <85047350+Upendra-uni-1729@users.noreply.github.com>
2026-01-21 12:59:49 +0000

    setup completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Product from "./pages/Product"
+import HomePage from "./pages/Homepage"
+import Pricing from "./pages/Pricing"
+import Login from "./pages/Login"
+
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+
+      </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
